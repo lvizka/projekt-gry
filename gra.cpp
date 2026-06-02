c0a4ea41590a920dba91ffb1753b93f7a8039db0
lvizka <luizakunicka10@gmail.com>
2026-06-02 10:37:55 +0200

    sporządzenie konceptu na gre, wpisanie początkowych komend

---
+++ gra.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Witaj w grze - ZBIERANIE SKARBOW "<<endl;
+    cout<<"autor: Luiza Kunicka"<<endl;
+    int start;
+    cout<<"1. Rozpocznij"<<endl;
+    cout<<"2. Wyjdz"<<endl;
+    cin>>start;
+
+    if(start=2){
+        break
+    }
+}
